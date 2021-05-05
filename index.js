f02e13fc5b6279f1f620e1b400d29f05678c60f1
Saloni <salonimehta2127@gmail.com>
2021-05-04 21:17:21 -0400

    commit tree ae562375ab8e1f64684e11ea0153c5172cecd195

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching= function(arrayOfDrivers,stringName)
+{
+
+ return arrayOfDrivers.filter((x)=> x.toLowerCase()==stringName.toLowerCase())
+}
+
+function fuzzyMatch(arrayOfDrivers,stringName)
+{
+return arrayOfDrivers.filter((x)=>x.charAt(0)==stringName.charAt(0))
+} 
+function matchName(arrayOfDriverObj,stringName)
+{
+return arrayOfDriverObj.filter((x)=> x.name.toLowerCase()==stringName.toLowerCase())
+}
